c14fd3768f68e493e63a221f9bad2e88cd4109a6
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-12-21 16:33:31 +0100

    terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { Observable, of } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ob1';
+export class AppComponent implements OnInit {
+  title: string = 'ob1';
+  posts$: Observable<any[]> = of([]);
+  constructor(private dataService: DataService) { }
+  ngOnInit(): void {
+    this.posts$ = this.dataService.getPosts();
+  }
 }
